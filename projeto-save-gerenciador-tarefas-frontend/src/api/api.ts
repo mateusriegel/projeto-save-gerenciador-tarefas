c77d15b48165aaeba40fce66027977a12c4d1808
Mateus <mateusriegel3@gmail.com>
2025-04-02 15:29:28 -0300

    ajuste url api

--- projeto-save-gerenciador-tarefas-frontend/src/api/api.ts
+++ projeto-save-gerenciador-tarefas-frontend/src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+//const API_URL = "http://localhost:3000/api";
+const API_URL = "https://projeto-save-gerenciador-tarefas-backend.onrender.com/api";
 
 export interface APIError {
   response?: {
