6f1e133a4bd9893e90c8761c99d901f9168b78a2
Mateus <mateusriegel3@gmail.com>
2025-04-02 17:47:11 -0300

    adicionado novamente método findAll tasks

--- projeto-save-gerenciador-tarefas-backend/src/routes/TaskRoutes.js
+++ projeto-save-gerenciador-tarefas-backend/src/routes/TaskRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { requireAuth } from '../middleware/AuthMiddleware.js';
-import { create, update, updateStatus, remove } from '../controllers/TaskController.js';
+import { findAll, create, update, updateStatus, remove } from '../controllers/TaskController.js';
 
 const router = express.Router();
 
 // Rotas privadas
+router.get('/', requireAuth, findAll);
 router.post('/', requireAuth, create);
 router.put('/:id', requireAuth, update);
 router.put('/:id/status', requireAuth, updateStatus);
